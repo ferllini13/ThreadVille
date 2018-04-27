fb715866c9cbbf2fcf4734ea8f42867a23f72e60
luisohz <luisohz@gmail.com>
2018-04-26 23:20:36 -0600

    cambio de bridges

--- main.c
+++ main.c
@@ -47,9 +47,160 @@ int main(int argc, char const *argv[]){
 	struct list rute11;
 	struct list rute12;
 	struct list rute13;
+	struct list rute14;
+	struct list rute15;
+	struct list rute16;
+	struct list rute17;
+	struct list rute18;
+	struct list rute19;
+	struct list rute20;
+	struct list rute21;
+	struct list rute22;
+	struct list rute23;
+	struct list rute24;
+	struct list rute25;
+	struct list rute26;
+
 
 	rute1.size = 1;
-	rute1.move = "nodeI21";
+	rute1.move = "nodeV14";
+	rute1.next = NULL;
+	rute1.finalPosition = 0;
+
+	rute2.size = 2;
+	rute2.move = "bridge3";
+	rute2.next = &rute1;
+	rute2.finalPosition = 0;
+
+	rute3.size = 3;
+	rute3.move = "nodeU14";
+	rute3.next = &rute2;
+	rute3.finalPosition = 0;
+	
+	rute4.size = 4;
+	rute4.move = "nodeT14";
+	rute4.next = &rute3;
+	rute4.finalPosition = 0;
+
+	rute5.size = 5;
+	rute5.move = "nodeS14";
+	rute5.next = &rute4;
+	rute5.finalPosition = 0;
+
+	rute6.size = 6;
+	rute6.move = "nodeS21";
+	rute6.next = &rute5;
+	rute6.finalPosition = 4;
+
+	rute7.size = 7;
+	rute7.move = "nodeS32";
+	rute7.next = &rute6;
+	rute7.finalPosition = 4;
+
+	rute8.size = 8;
+	rute8.move = "nodeS43T12";
+	rute8.next = &rute7;
+	rute8.finalPosition = 4;
+
+	rute9.size = 9;
+	rute9.move = "nodeM43N12";
+	rute9.next = &rute8;
+	rute9.finalPosition = 4;
+
+	rute10.size = 10;
+	rute10.move = "nodeG43H12";
+	rute10.next = &rute9;
+	rute10.finalPosition = 4;
+
+	rute11.size = 11;
+	rute11.move = "nodeG14";
+	rute11.next = &rute10;
+	rute11.finalPosition = 4;
+
+	rute12.size = 12;
+	rute12.move = "nodeG21";
+	rute12.next = &rute11;
+	rute12.finalPosition = 4;
+
+	rute13.size = 13;
+	rute13.move = "nodeG32";
+	rute13.next = &rute12;
+	rute13.finalPosition = 4;
+
+	rute14.size = 14;
+	rute14.move = "nodeH32";
+	rute14.next = &rute13;
+	rute14.finalPosition = 4;
+
+	rute15.size = 15;
+	rute15.move = "nodeI32";
+	rute15.next = &rute14;
+	rute15.finalPosition = 4;
+
+	rute16.size = 16;
+	rute16.move = "bridge2";
+	rute16.next = &rute15;
+	rute16.finalPosition = 4;
+
+	rute17.size = 17;
+	rute17.move = "nodeJ32";
+	rute17.next = &rute16;
+	rute17.finalPosition = 4;
+
+	rute18.size = 18;
+	rute18.move = "nodeJ43K12";
+	rute18.next = &rute17;
+	rute18.finalPosition = 4;
+
+	rute19.size = 19;
+	rute19.move = "nodeJ14";
+	rute19.next = &rute18;
+	rute19.finalPosition = 4;
+
+	rute20.size = 20;
+	rute20.move = "bridge1";
+	rute20.next = &rute19;
+	rute20.finalPosition = 4;
+
+	rute21.size = 21;
+	rute21.move = "nodeI14";
+	rute21.next = &rute20;
+	rute21.finalPosition = 4;
+
+	rute22.size = 22;
+	rute22.move = "nodeH14";
+	rute22.next = &rute21;
+	rute22.finalPosition = 4;
+
+	rute23.size = 23;
+	rute23.move = "nodeG14";
+	rute23.next = &rute22;
+	rute23.finalPosition = 4;
+
+	rute24.size = 24;
+	rute24.move = "nodeG21";
+	rute24.next = &rute23;
+	rute24.finalPosition = 4;
+
+	rute25.size = 25;
+	rute25.move = "nodeG32";
+	rute25.next = &rute24;
+	rute25.finalPosition = 4;
+
+	rute26.size = 26;
+	rute26.move = "nodeG43H12";
+	rute26.next = &rute25;
+	rute26.finalPosition = 4;
+
+
+	struct vehicle *car;
+	car = create(0, 1, 2, 0, 0, actualNode, NULL, NULL, NULL, rute26, "", "", "nodeA43B12");
+
+
+	
+/*
+	rute1.size = 1;
+	rute1.move = "nodeH34I21";
 	rute1.next = NULL;
 	rute1.finalPosition = 0;
 
@@ -114,12 +265,78 @@ int main(int argc, char const *argv[]){
 	rute13.finalPosition = 4;
 
 
+	struct vehicle *car;
+	car = create(0, 1, 2, 0, 0, actualNode, NULL, NULL, NULL, rute13, "", "", "nodeA43B12");
+
+*/	
+
+
+/*
+	rute1.size = 1;
+	rute1.move = "nodeI32";
+	rute1.next = NULL;
+	rute1.finalPosition = 0;
+
+	rute2.size = 2;
+	rute2.move = "bridge2";
+	rute2.next = &rute1;
+	rute2.finalPosition = 0;
+
+	rute3.size = 3;
+	rute3.move = "nodeJ32";
+	rute3.next = &rute2;
+	rute3.finalPosition = 0;
+	
+	rute4.size = 4;
+	rute4.move = "nodeJ43K12";
+	rute4.next = &rute3;
+	rute4.finalPosition = 0;
+
+	rute5.size = 5;
+	rute5.move = "nodeJ14";
+	rute5.next = &rute4;
+	rute5.finalPosition = 0;
+
+	rute6.size = 6;
+	rute6.move = "bridge1";
+	rute6.next = &rute5;
+	rute6.finalPosition = 4;
+
+	rute7.size = 7;
+	rute7.move = "nodeI14";
+	rute7.next = &rute6;
+	rute7.finalPosition = 4;
+
+	rute8.size = 8;
+	rute8.move = "nodeH14";
+	rute8.next = &rute7;
+	rute8.finalPosition = 4;
+
+	rute9.size = 9;
+	rute9.move = "nodeG14";
+	rute9.next = &rute8;
+	rute9.finalPosition = 4;
+
+	rute10.size = 10;
+	rute10.move = "nodeG21";
+	rute10.next = &rute9;
+	rute10.finalPosition = 4;
+
+	rute11.size = 11;
+	rute11.move = "nodeG32";
+	rute11.next = &rute10;
+	rute11.finalPosition = 4;
+
+	rute12.size = 12;
+	rute12.move = "nodeG43H12";
+	rute12.next = &rute11;
+	rute12.finalPosition = 0;
 
 
 
 	struct vehicle *car;
-	car = create(0, 1, 2, 0, 0, actualNode, NULL, NULL, NULL, rute13, "", "", "nodeA43B12");
-
+	car = create(0, 1, 2, 0, 0, actualNode, NULL, NULL, NULL, rute12, "", "", "nodeA43B12");
+*/
 	move(car);
 
 
--- src/vehicle.c
+++ src/vehicle.c
@@ -57,7 +57,8 @@ void move(vehicle * car){
 		printf("car->rute.move: %s\n\n\n", car->rute.move);
 		//printf("car->rute.size: %d\n\n\n", car->rute.size);
 
-		sleep(1);
+		// if( car->rute.size < 2)
+			sleep(1);
 		
 	// char* nameNextNodeStraight2 = car->actualNode->nextStraight->name;
 	// char* nameNextNodeRight2 = car->actualNode->nextRight->name;
@@ -150,25 +151,368 @@ void move(vehicle * car){
 				}
 				
 			}
-		} else if(car->rute.size > 0 && car->position < 15 && car->actualNodeR != NULL){ //For roundabouts
-			//printf("***************************************************ROUNDABOUT 0\n");
-			if(car->actualNodeR->Vehicles[car->position + 1] == NULL){
-				car->actualNodeR->Vehicles[car->position + 1] = car;
-				car->actualNodeR->Vehicles[car->position] = NULL;
-				car->position = car->position + 1;
-			}			
-		} else if(car->rute.size > 0 && car->position < 20 && car->actualNodeH != NULL){ //For Highways
-			//printf("***************************************************HIGHWAY 0\n");
-			if(car->actualNodeH->Vehicles[car->position + 1] == NULL){
-				car->actualNodeH->Vehicles[car->position + 1] = car;
-				car->actualNodeH->Vehicles[car->position] = NULL;
-				car->position = car->position + 1;
-			}	
+		// } else if(car->rute.size > 0 && car->position < 15 && car->actualNodeR != NULL){ //For roundabouts
+		// 	printf("***************************************************ROUNDABOUT 0\n");
+		// 	if(car->actualNodeR->Vehicles[car->position + 1] == NULL){
+		// 		car->actualNodeR->Vehicles[car->position + 1] = car;
+		// 		car->actualNodeR->Vehicles[car->position] = NULL;
+		// 		car->position = car->position + 1;
+		// 	}			
+		// } else if(car->rute.size > 0 && car->position < 20 && car->actualNodeH != NULL){ //For Highways
+		// 	printf("***************************************************HIGHWAY 0\n");
+		// 	if(car->actualNodeH->Vehicles[car->position + 1] == NULL){
+		// 		car->actualNodeH->Vehicles[car->position + 1] = car;
+		// 		car->actualNodeH->Vehicles[car->position] = NULL;
+		// 		car->position = car->position + 1;
+		// 	}	
 		} 
 
+//**********************************************************************************************
+
+		else if(car->rute.size == 0 && car->position >= 4 && car->actualNode != NULL){  //From normal roads to roads, bridges or roundabouts
+			
+			int cmp = 0;
+			
+			char* nameNextNode = car->nextNode;
 
 
-		  else if(car->rute.size == 0 && car->position >= 4){
+			lastNodeName = car->actualNode->name;
+
+			char *nextBridge1East = "nodeI14";
+			char *nextBridge1West = "nodeD32";
+			char *nextBridge2East = "nodeO14";
+			char *nextBridge2West = "nodeJ32";
+			char *nextBridge3East = "nodeU14";
+			char *nextBridge3West = "nodeP32";
+
+			int cmpB1E= 0;
+			int cmpB1W = 0;
+			int cmpB2E= 0;
+			int cmpB2W = 0;
+			int cmpB3E= 0;
+			int cmpB3W = 0;
+
+			char *nextRY = "roundaboutY";
+			char *nextRZ = "roundaboutZ";
+
+			int cmpRY = 0;
+			int cmpRZ = 0;
+
+			char *nextB1 = "bridge1";
+			char *nextB2 = "bridge2";
+			char *nextB3 = "bridge3";
+
+			int cmpB1 = 0;
+			int cmpB2 = 0;
+			int cmpB3 = 0;
+
+			// char *nextHSN1 = "highwaySN1";
+			// char *nextHSN2 = "highwaySN2";
+			// char *nextHNS1 = "highwayNS1";
+			// char *nextHNS2 = "highwayNS2";
+
+			// int cmpHSN1 = 0;
+			// int cmpHSN2 = 0;
+			// int cmpHNS1 = 0;
+			// int cmpHNS2 = 0;
+
+			cmpB1E = strcmp(lastNodeName, nextBridge1East); //0 is =
+			cmpB1W = strcmp(lastNodeName, nextBridge1West); //0 is =
+			cmpB2E = strcmp(lastNodeName, nextBridge2East); //0 is =
+			cmpB2W = strcmp(lastNodeName, nextBridge2West); //0 is =
+			cmpB3E = strcmp(lastNodeName, nextBridge3East); //0 is =
+			cmpB3W = strcmp(lastNodeName, nextBridge3West); //0 is =
+
+			cmpRY = strcmp(nameNextNode, nextRY); //0 is =
+			cmpRZ = strcmp(nameNextNode, nextRZ); //0 is =
+
+			cmpB1 = strcmp(nameNextNode, nextB1); //0 is =
+			cmpB2 = strcmp(nameNextNode, nextB2); //0 is =
+			cmpB3 = strcmp(nameNextNode, nextB3); //0 is =
+
+			// cmpHSN1 = strcmp(nameNextNode, nextHSN1); //0 is =
+			// cmpHSN2 = strcmp(nameNextNode, nextHSN2); //0 is =
+			// cmpHNS1 = strcmp(nameNextNode, nextHNS1); //0 is =
+			// cmpHNS2 = strcmp(nameNextNode, nextHNS2); //0 is =
+
+
+			if(cmpRY == 0 || cmpRZ == 0){ //From road to roundabout
+
+			} else if(cmpB1 == 0 || cmpB2 == 0 || cmpB3 == 0){ //From road to bridge
+
+				if(cmpB1E == 0 && car->actualNode->nextBridge->flagEast == 1){
+
+					if(car->actualNode->nextBridge->Vehicles[0] == NULL){
+						car->actualNode->nextBridge->Vehicles[0] = car;
+						car->actualNode->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNodeB = car->actualNode->nextBridge;
+						car->actualNode = NULL;
+					}
+
+					//Update the rute information
+					car->nextNode = car->rute.move;
+					if(car->rute.size - 1 == 0){
+						car->rute.size = 0;
+						car->rute.move = NULL;
+					} else {
+						car->rute.move = car->rute.next->move;
+						car->rute.next = car->rute.next->next;
+						car->rute.size = car->rute.size - 1;
+					}
+
+				} else if(cmpB1W == 0 && car->actualNode->nextBridge->flagWest == 1){
+
+					if(car->actualNode->nextBridge->Vehicles[4] == NULL){
+
+						car->actualNode->nextBridge->Vehicles[4] = car;
+						car->actualNode->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNodeB = car->actualNode->nextBridge;
+						car->actualNode = NULL;
+					}
+
+					//Update the rute information
+					car->nextNode = car->rute.move;
+					if(car->rute.size - 1 == 0){
+						car->rute.size = 0;
+						car->rute.move = NULL;
+					} else {
+						car->rute.move = car->rute.next->move;
+						car->rute.next = car->rute.next->next;
+						car->rute.size = car->rute.size - 1;
+					}
+
+				} else if(cmpB2E == 0 && car->actualNode->nextBridge->flagEast == 1){
+
+					if(car->actualNode->nextBridge->Vehicles[0] == NULL){
+						car->actualNode->nextBridge->Vehicles[0] = car;
+						car->actualNode->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNodeB = car->actualNode->nextBridge;
+						car->actualNode = NULL;
+					}
+
+					//Update the rute information
+					car->nextNode = car->rute.move;
+					if(car->rute.size - 1 == 0){
+						car->rute.size = 0;
+						car->rute.move = NULL;
+					} else {
+						car->rute.move = car->rute.next->move;
+						car->rute.next = car->rute.next->next;
+						car->rute.size = car->rute.size - 1;
+					}
+
+				} else if(cmpB2W == 0 && car->actualNode->nextBridge->flagWest == 1){
+
+					if(car->actualNode->nextBridge->Vehicles[4] == NULL){
+
+						car->actualNode->nextBridge->Vehicles[4] = car;
+						car->actualNode->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNodeB = car->actualNode->nextBridge;
+						car->actualNode = NULL;
+					}
+
+					//Update the rute information
+					car->nextNode = car->rute.move;
+					if(car->rute.size - 1 == 0){
+						car->rute.size = 0;
+						car->rute.move = NULL;
+					} else {
+						car->rute.move = car->rute.next->move;
+						car->rute.next = car->rute.next->next;
+						car->rute.size = car->rute.size - 1;
+					}
+
+				} else if(cmpB3E == 0 && car->actualNode->nextBridge->flagEast == 1){
+
+					if(car->actualNode->nextBridge->Vehicles[0] == NULL){
+
+						car->actualNode->nextBridge->Vehicles[0] = car;
+						car->actualNode->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNodeB = car->actualNode->nextBridge;
+						car->actualNode = NULL;
+					}
+
+					//Update the rute information
+					car->nextNode = car->rute.move;
+					if(car->rute.size - 1 == 0){
+						car->rute.size = 0;
+						car->rute.move = NULL;
+					} else {
+						car->rute.move = car->rute.next->move;
+						car->rute.next = car->rute.next->next;
+						car->rute.size = car->rute.size - 1;
+					}
+
+				} else if(cmpB3W == 0 && car->actualNode->nextBridge->flagWest == 1){
+
+					if(car->actualNode->nextBridge->Vehicles[4] == NULL){
+
+						car->actualNode->nextBridge->Vehicles[4] = car;
+						car->actualNode->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNodeB = car->actualNode->nextBridge;
+						car->actualNode = NULL;
+					}
+
+					//Update the rute information
+					car->nextNode = car->rute.move;
+					if(car->rute.size - 1 == 0){
+						car->rute.size = 0;
+						car->rute.move = NULL;
+					} else {
+						car->rute.move = car->rute.next->move;
+						car->rute.next = car->rute.next->next;
+						car->rute.size = car->rute.size - 1;
+					}
+				}
+
+				printf("***************************************************BRIDGE\n");
+
+
+			// } else if(cmpHSN1 == 0 || cmpHSN2 == 0 || cmpHNS1 == 0 || cmpHNS2 == 0){
+
+			// 	if(car->actualNodeR->nextRight1->Vehicles[0] == NULL){
+			// 		car->actualNodeR->nextRight1->Vehicles[0] = car;
+			// 		car->actualNodeR->Vehicles[0] = NULL;
+			// 		car->position = 0;
+			// 		car->actualNodeR = NULL;
+			// 		car->actualNodeH = car->actualNodeR->nextRight1;
+			// 	}
+
+			
+			} else { //From road to road
+
+				if(car->actualNode->nextStraight == NULL){
+					char* nameNextNodeRight = car->actualNode->nextRight->name;
+					cmp = strcmp(nameNextNode, nameNextNodeRight); //0 is =
+					if(cmp==0)
+						cmp = 1;
+				} else if(car->actualNode->nextRight == NULL){
+					char* nameNextNodeStraight = car->actualNode->nextStraight->name;
+					cmp = strcmp(nameNextNode, nameNextNodeStraight); //0 is =
+				} else {
+					char* nameNextNodeStraight = car->actualNode->nextStraight->name;
+					char* nameNextNodeRight = car->actualNode->nextRight->name;
+					cmp = strcmp(nameNextNode, nameNextNodeStraight); //0 is =
+				}
+
+				switch(cmp){
+					case 0: //nameNextNodeStraight
+						if(car->actualNode->nextStraight->Vehicles[0] == NULL){
+							car->actualNode->nextStraight->Vehicles[0] = car;
+							car->actualNode->Vehicles[4] = NULL;
+							car->position = 0;
+							car->actualNode = car->actualNode->nextStraight;
+						}
+						break;
+					default: //nameNextNodeRight
+						if(car->actualNode->nextRight->Vehicles[0] == NULL){
+							car->actualNode->nextRight->Vehicles[0] = car;
+							car->actualNode->Vehicles[4] = NULL;
+							car->position = 0;
+							car->actualNode = car->actualNode->nextRight;
+						}
+						break;
+				}
+
+				//Update the rute information
+				car->nextNode = NULL;
+				if(car->rute.size - 1 == -1){
+					car->rute.size = -1;
+				}
+
+			}
+			// int cmp2 = 0;
+			// if(car->actualNode->nextStraight == NULL){
+			// 	char* nameNextNodeRight2 = car->actualNode->nextRight->name;
+			// 	char* nameNextNode2 = car->nextNode;
+			// 	cmp2 = strcmp(nameNextNode2, nameNextNodeRight2); //0 is =
+			// 	if(cmp2 == 0)
+			// 		cmp2 = 1;
+			// } else if(car->actualNode->nextRight == NULL){
+			// 	char* nameNextNodeStraight2 = car->actualNode->nextStraight->name;
+			// 	char* nameNextNode2 = car->nextNode;
+			// 	cmp2 = strcmp(nameNextNode2, nameNextNodeStraight2); //0 is =
+			// } else {
+			// 	char* nameNextNodeStraight2 = car->actualNode->nextStraight->name;
+			// 	char* nameNextNodeRight2 = car->actualNode->nextRight->name;
+			// 	char* nameNextNode2 = car->nextNode;
+			// 	cmp2 = strcmp(nameNextNode2, nameNextNodeStraight2); //0 is =
+			// }
+			
+
+			// // printf("nameNextNodeStraight2: %s\n", nameNextNodeStraight2);
+			// // printf("nameNextNodeRight2: %s\n", nameNextNodeRight2);
+			// // printf("nameNextNode2: %s\n\n\n", nameNextNode2);
+			
+			// switch(cmp2){
+			// 	case 0: //nameNextNodeStraight
+			// 		if(car->actualNode->nextStraight->Vehicles[0] == NULL){
+			// 			car->actualNode->nextStraight->Vehicles[0] = car;
+			// 			car->actualNode->Vehicles[4] = NULL;
+			// 			car->position = 0;
+			// 			car->actualNode = car->actualNode->nextStraight;
+			// 		}
+			// 		break;
+			// 	default: //nameNextNodeRight
+			// 		if(car->actualNode->nextRight->Vehicles[0] == NULL){
+			// 			car->actualNode->nextRight->Vehicles[0] = car;
+			// 			car->actualNode->Vehicles[4] = NULL;
+			// 			car->position = 0;
+			// 			car->actualNode = car->actualNode->nextRight;
+			// 		}
+			// 		break;
+			// }
+
+			// //Update the rute information
+			// car->nextNode = NULL;
+			// if(car->rute.size - 1 == -1){
+			// 	car->rute.size = -1;
+			// }
+
+		} else if(car->rute.size == 0 && car->position >= 4 && car->actualNodeB != NULL){ //From bridges to roads
+			int cmp = 0;
+			char* nameNextNodeWest = car->actualNodeB->nextWest->name;
+			char* nameNextNodeEast = car->actualNodeB->nextEast->name;
+			char* nameNextNode = car->nextNode;
+			cmp = strcmp(nameNextNode, nameNextNodeWest); //0 is =			
+
+			switch(cmp){
+				case 0: //nameNextNodeWest
+					if(car->actualNodeB->nextWest->Vehicles[0] == NULL){
+						car->actualNodeB->nextWest->Vehicles[0] = car;
+						car->actualNodeB->Vehicles[4] = NULL;
+						car->position = 0;
+						car->actualNode = car->actualNodeB->nextWest;
+						car->actualNodeB = NULL;
+					}
+					break;
+				default: //nameNextNodeEast
+					if(car->actualNodeB->nextEast->Vehicles[0] == NULL){
+						car->actualNodeB->nextEast->Vehicles[0] = car;
+						car->actualNodeB->Vehicles[0] = NULL;
+						car->position = 0;
+						car->actualNode = car->actualNodeB->nextEast;
+						car->actualNodeB = NULL;
+					}
+					break;
+			}
+
+			//Update the rute information
+			car->nextNode = NULL;
+			if(car->rute.size - 1 == -1){
+				car->rute.size = -1;
+			}
+		}
+
+//**********************************************************************************************
+
+		 /* else if(car->rute.size == 0 && car->position >= 4){
 			int cmp2 = 0;
 			if(car->actualNode->nextStraight == NULL){
 				char* nameNextNodeRight2 = car->actualNode->nextRight->name;
@@ -188,9 +532,9 @@ void move(vehicle * car){
 			}
 			
 
-			// printf("nameNextNodeStraight2: %s\n", nameNextNodeStraight2);
-			// printf("nameNextNodeRight2: %s\n", nameNextNodeRight2);
-			// printf("nameNextNode2: %s\n\n\n", nameNextNode2);
+			printf("nameNextNodeStraight2: %s\n", nameNextNodeStraight2);
+			printf("nameNextNodeRight2: %s\n", nameNextNodeRight2);
+			printf("nameNextNode2: %s\n\n\n", nameNextNode2);
 			
 			switch(cmp2){
 				case 0: //nameNextNodeStraight
@@ -224,6 +568,8 @@ void move(vehicle * car){
 		  //If is the last trip
 		} 
 
+		*/
+
 
 
 
@@ -243,8 +589,8 @@ void move(vehicle * car){
 					printf("car->actualNode->name: %s\n", car->actualNode->name);
 					printf("car->nextNode: %s\n", car->nextNode);
 					printf("car->rute.move: %s\n\n\n", car->rute.move);
-					//printf("car->rute.size: %d\n\n\n", car->rute.size);
-					//printf("car->rute.finalPosition: %d\n\n\n", car->rute.finalPosition);
+					// printf("car->rute.size: %d\n\n\n", car->rute.size);
+					// printf("car->rute.finalPosition: %d\n\n\n", car->rute.finalPosition);
 				}
 				
 				if(car->actualNode->Vehicles[car->position + 1] == NULL){
